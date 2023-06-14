38dd54c6879d2b10b42690e72f3fd2910bf4ac3f
JaimeSoftDev <jaimesoftdev@gmail.com>
2023-06-14 23:08:16 +0200

    Versión refactorizada

--- geometria/src/Circulo_JMM.java
+++ geometria/src/Circulo_JMM.java
@@ -1,10 +1,10 @@
 
-public class Circulo extends FiguraGeometrica {
+public class Circulo_JMM extends FiguraGeometrica_JMM {
 	private double radio;
 	//Constante pi
 	static final double PI = 3.1416;
 	
-	public Circulo(double r, String tipoFigura) {
+	public Circulo_JMM(double r, String tipoFigura) {
 		super(tipoFigura);
 		radio = r;
 	}
--- geometria/src/FiguraGeometrica_JMM.java
+++ geometria/src/FiguraGeometrica_JMM.java
@@ -1,9 +1,9 @@
 
-public abstract class FiguraGeometrica {
+public abstract class FiguraGeometrica_JMM {
 
 	protected String tipoFigura;
 
-	public FiguraGeometrica(String tipoFigura) {
+	public FiguraGeometrica_JMM(String tipoFigura) {
 		super();
 		this.tipoFigura = tipoFigura;
 	}
