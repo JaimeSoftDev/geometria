a9bebe6ee0dcb33d495f0fe785547bb3ed1cb493
JaimeSoftDev <jaimesoftdev@gmail.com>
2023-06-14 23:12:41 +0200

    verisón comentada

--- geometria/src/Rectangulo_JMM.java
+++ geometria/src/Rectangulo_JMM.java
@@ -1,4 +1,8 @@
-
+/**
+ * 
+ * @author Jaime Maciá Mora
+ *@version 1.2
+ */
 public class Rectangulo_JMM extends FiguraGeometrica_JMM {
 	private double l1;
 	private double l2;
@@ -8,12 +12,16 @@ public class Rectangulo_JMM extends FiguraGeometrica_JMM {
 		l1 = lG;
 		l2 = lP;
 	}
-
+	/**
+	 * @return devuelve el área en un double
+	 */
 	@Override
 	public double area() {
 		return l1 * l2;
 	}
-	
+	/**
+	 * @return devuelve el perímetro en un double
+	 */
 	@Override
 	public double perimetro() {
 		return (2 * l1) + (2 * l2);
